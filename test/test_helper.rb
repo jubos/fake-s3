7e0329188ac7ed7092e97deae6479d417971a036
Nick Gauthier <ngauthier@gmail.com>
2012-04-18 15:02:33 -0400

    load path via bundler in test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
 require 'test/unit'
 require 'bundler/setup'
-
-testdir = File.dirname(__FILE__)
-$LOAD_PATH.unshift testdir unless $LOAD_PATH.include?(testdir)
-
-libdir = File.dirname(File.dirname(__FILE__)) + '/lib'
-$LOAD_PATH.unshift libdir unless $LOAD_PATH.include?(libdir)
+require 'fakes3'
