13a8c45be86a72ca087a49aa5346b7cf9c5e4b1d
Kieran Elliott <kieran@goclio.com>
2012-07-01 12:41:12 -0600

    refactored RateLimitableFile to avoid creating a file handle
    the server was reaching it's alloted file handle limit during a heavy unit test.
    When a openning a bucket for the first time, the server creates an object representing
    each file found. Part of creating each object involves openning a file handle to the
    uploaded content. Refactored to lazily open the file when the content is actually
    requested.

--- lib/fakes3/file_store.rb
+++ lib/fakes3/file_store.rb
@@ -109,7 +109,7 @@ module FakeS3
         real_obj.md5 = metadata[:md5]
         real_obj.content_type = metadata.fetch(:content_type) { "application/octet-stream" }
         #real_obj.io = File.open(File.join(obj_root,"content"),'rb')
-        real_obj.io = RateLimitableFile.open(File.join(obj_root,"content"),'rb')
+        real_obj.io = RateLimitableFile.new(File.join(obj_root,"content"))
         real_obj.creation_date = File.ctime(obj_root)
         real_obj.modified_date = File.mtime(File.join(obj_root,"content"))
         return real_obj
