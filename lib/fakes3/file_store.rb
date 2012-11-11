ca42dcab93b1e9b1b18366af9e8187dc53cd429b
Thomas Walpole <twalpole@gmail.com>
2012-11-11 13:28:17 -0800

    basic post support

--- lib/fakes3/file_store.rb
+++ lib/fakes3/file_store.rb
@@ -151,14 +151,25 @@ module FakeS3
 
         md5 = Digest::MD5.new
         # TODO put a tmpfile here first and mv it over at the end
-
-        File.open(content,'wb') do |f|
-          request.body do |chunk|
-            f << chunk
-            md5 << chunk
+        
+        match=request.content_type.match(/^multipart\/form-data; boundary=(.+)/)
+      	boundary = match[1] if match
+        if boundary
+          boundary = WEBrick::HTTPUtils::dequote(boundary)
+          filedata = WEBrick::HTTPUtils::parse_form_data(request.body, boundary)
+          raise HTTPStatus::BadRequest if filedata['file'].empty?
+          File.open(content, 'wb') do |f|
+            f<<filedata['file']
+            md5<<filedata['file']
+          end
+        else
+          File.open(content,'wb') do |f|
+            request.body do |chunk|
+              f << chunk
+              md5 << chunk
+            end
           end
         end
-
         metadata_struct = {}
         metadata_struct[:md5] = md5.hexdigest
         metadata_struct[:content_type] = request.header["content-type"].first
---
+++ test/post_test.rb
@@ -0,0 +1,51 @@
+require 'test/test_helper'
+require 'debugger'
+require 'rest-client'
+
+class PostTest < Test::Unit::TestCase
+  def setup
+    @url='http://posttest.localhost:10453/'
+  end
+
+  def teardown
+  end
+
+  def test_options
+    res=RestClient.options( @url ) { |response|
+      assert_equal(response.headers[:access_control_allow_origin],"*")
+    }
+  end
+  
+  def test_redirect
+    res=RestClient.post( @url,
+      'key'=>'uploads/12345/${filename}',
+      'success_action_redirect'=>'http://somewhere.else.com/',
+      'file'=>File.new(__FILE__,"rb")
+      ){ |response| 
+        assert_equal(response.code, 307)
+        assert_equal(response.headers[:location], 'http://somewhere.else.com/') 
+    }
+  end
+  
+  def test_status_200
+    res=RestClient.post( @url,
+      'key'=>'uploads/12345/${filename}',
+      'success_action_status'=>'200',
+      'file'=>File.new(__FILE__,"rb")
+      ){ |response| 
+        assert_equal(response.code, 200)
+    }
+  end
+
+  def test_status_201
+    res=RestClient.post( @url,
+      'key'=>'uploads/12345/${filename}',
+      'success_action_status'=>'201',
+      'file'=>File.new(__FILE__,"rb")
+      ){ |response| 
+        assert_equal(response.code, 201)
+        assert_match(response.body, /uploads\/12345\/post_test.rb/)
+    }
+  end
+
+end
