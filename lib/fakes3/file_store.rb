3e2f69e846e4afbba3a3bd09b79439c328040263
Vadim Spivak <vadim@spivak.net>
2013-11-05 23:00:14 -0800

    Date formatting compatibility fixes
    The Java AWS SDK expects the Last-Modified header to be in RFC 2616
    format. It also expects the rest of the dates to use UTC for the
    timezone.

--- lib/fakes3/file_store.rb
+++ lib/fakes3/file_store.rb
@@ -82,8 +82,8 @@ module FakeS3
         #real_obj.io = File.open(File.join(obj_root,"content"),'rb')
         real_obj.io = RateLimitableFile.open(File.join(obj_root,"content"),'rb')
         real_obj.size = metadata.fetch(:size) { 0 }
-        real_obj.creation_date = File.ctime(obj_root).iso8601()
-        real_obj.modified_date = metadata.fetch(:modified_date) { File.mtime(File.join(obj_root,"content")).iso8601() }
+        real_obj.creation_date = File.ctime(obj_root).utc.iso8601()
+        real_obj.modified_date = metadata.fetch(:modified_date) { File.mtime(File.join(obj_root,"content")).utc.iso8601() }
         return real_obj
       rescue
         puts $!
@@ -163,7 +163,7 @@ module FakeS3
         metadata_struct[:md5] = md5.hexdigest
         metadata_struct[:content_type] = request.header["content-type"].first
         metadata_struct[:size] = File.size(content)
-        metadata_struct[:modified_date] = File.mtime(content).iso8601()
+        metadata_struct[:modified_date] = File.mtime(content).utc.iso8601()
 
         File.open(metadata,'w') do |f|
           f << YAML::dump(metadata_struct)
