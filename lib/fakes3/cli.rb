2649cc26dda4ada8fd04c9301565eb31255d3f73
Andrew Gaul <andrew@gaul.org>
2014-08-24 17:21:54 -0700

    Bind "H" short option to --hostname
    fake-s3 already binds "h" to --help:
    
    $ fakes3 -h 127.0.0.1 -p 8080 -r /tmp/fakes3/
    ERROR: "fakes3 help" was called with arguments ["127.0.0.1", "-p", "8080", "-r", "/tmp/fakes3/"]
    Usage: "fakes3 help [COMMAND]"

--- lib/fakes3/cli.rb
+++ lib/fakes3/cli.rb
@@ -10,7 +10,7 @@ module FakeS3
     method_option :root, :type => :string, :aliases => '-r', :required => true
     method_option :port, :type => :numeric, :aliases => '-p', :required => true
     method_option :address, :type => :string, :aliases => '-a', :required => false, :desc => "Bind to this address. Defaults to 0.0.0.0"
-    method_option :hostname, :type => :string, :aliases => '-h', :desc => "The root name of the host.  Defaults to s3.amazonaws.com."
+    method_option :hostname, :type => :string, :aliases => '-H', :desc => "The root name of the host.  Defaults to s3.amazonaws.com."
     method_option :limit, :aliases => '-l', :type => :string, :desc => 'Rate limit for serving (ie. 50K, 1.0M)'
     method_option :sslcert, :type => :string, :desc => 'Path to SSL certificate'
     method_option :sslkey, :type => :string, :desc => 'Path to SSL certificate key'
