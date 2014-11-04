8422090ae25fd4a57725403d346badb546aa8e81
Matt Butterfield <matt.butterfield@percolate.com>
2014-11-04 14:25:32 -0500

    add support for custom metadata

--- lib/fakes3/file_store.rb
+++ lib/fakes3/file_store.rb
@@ -84,6 +84,7 @@ module FakeS3
         real_obj.size = metadata.fetch(:size) { 0 }
         real_obj.creation_date = File.ctime(obj_root).utc.iso8601()
         real_obj.modified_date = metadata.fetch(:modified_date) { File.mtime(File.join(obj_root,"content")).utc.iso8601() }
+        real_obj.custom_metadata = metadata.fetch(:custom_metadata) { {} }
         return real_obj
       rescue
         puts $!
@@ -162,7 +163,7 @@ module FakeS3
         # TODO put a tmpfile here first and mv it over at the end
 
         match=request.content_type.match(/^multipart\/form-data; boundary=(.+)/)
-      	boundary = match[1] if match
+        boundary = match[1] if match
         if boundary
           boundary = WEBrick::HTTPUtils::dequote(boundary)
           filedata = WEBrick::HTTPUtils::parse_form_data(request.body, boundary)
@@ -217,6 +218,14 @@ module FakeS3
       metadata[:content_type] = request.header["content-type"].first
       metadata[:size] = File.size(content)
       metadata[:modified_date] = File.mtime(content).utc.iso8601()
+      metadata_struct[:custom_metadata] = {}
+
+      request.header.each do |key, value|
+        match = /^x-amz-meta-(.*)$/.match(key)
+        if match
+          metadata_struct[:custom_metadata][match[1]] = value.join(', ')
+        end
+      end
       return metadata
     end
   end
--- lib/fakes3/s3_object.rb
+++ lib/fakes3/s3_object.rb
@@ -1,7 +1,7 @@
 module FakeS3
   class S3Object
     include Comparable
-    attr_accessor :name,:size,:creation_date,:modified_date,:md5,:io,:content_type
+    attr_accessor :name,:size,:creation_date,:modified_date,:md5,:io,:content_type,:custom_metadata
 
     def hash
       @name.hash
