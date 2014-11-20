86b320606444442fe45581a538bf4d44c9c4594c
David Fernandez <david.fernandez@gatemedia.ch>
2014-11-20 14:38:05 +0100

    Fix FakeS3::FileStore#copy_object: the src object should not be removed

---
+++ test/aws_sdk_commands_test.rb
@@ -0,0 +1,23 @@
+require 'test/test_helper'
+require 'aws-sdk-v1'
+
+class AwsSdkCommandsTest < Test::Unit::TestCase
+  def setup
+    @s3 = AWS::S3.new(:access_key_id => '123',
+                      :secret_access_key => 'abc',
+                      :s3_endpoint => 'localhost',
+                      :s3_port => 10453,
+                      :use_ssl => false)
+  end
+
+  def test_copy_to
+    bucket = @s3.buckets["test_copy_to"]
+    object = bucket.objects["key1"]
+    object.write("asdf")
+
+    assert object.exists?
+    object.copy_to("key2")
+
+    assert_equal 2, bucket.objects.count
+  end
+end
