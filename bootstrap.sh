dc38fd3dbbf1b343764918d65eedb98425f36bec
Omar Khan <omar@omarkhan.me>
2015-02-17 18:11:01 +0700

    Fix hosts file when bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ if ! grep -q 'cd \/vagrant' /home/vagrant/.bashrc; then
 fi
 cd /vagrant
 
+# Fix hosts file
+echo '127.0.0.1 posttest.localhost' | sudo tee /etc/hosts
+
 # Install dependencies
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update --quiet
