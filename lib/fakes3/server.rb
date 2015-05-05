519cfea18df49722526669cf20b29dafd4f128dc
Sebastian Eichner <mail@sebastian-eichner.de>
2015-05-05 13:40:26 +0200

    use ruby-1.8 compatible hash

--- lib/fakes3/server.rb
+++ lib/fakes3/server.rb
@@ -486,8 +486,8 @@ module FakeS3
 
       parts_xml.collect do |xml|
         {
-          number: xml[/\<PartNumber\>(\d+)\<\/PartNumber\>/, 1].to_i,
-          etag:   xml[/\<ETag\>\"(.+)\"\<\/ETag\>/, 1]
+          :number => xml[/\<PartNumber\>(\d+)\<\/PartNumber\>/, 1].to_i,
+          :etag   => xml[/\<ETag\>\"(.+)\"\<\/ETag\>/, 1]
         }
       end
     end
