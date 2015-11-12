da2ffcb0f64f8d8adb427caf4df4cd75c13412c1
Stuart Hicks <stuarthicks@users.noreply.github.com>
2015-11-12 11:42:53 +0000

    test amazon vs custom metadata getting written into metadata file separately

--- test/aws_sdk_commands_test.rb
+++ test/aws_sdk_commands_test.rb
@@ -28,4 +28,32 @@ class AwsSdkCommandsTest < Test::Unit::TestCase
     assert object.exists?
     assert_equal "thisisaverybigfile", object.read
   end
+
+  def test_metadata
+    file_path = './test_root/test_metadata/metaobject'
+    FileUtils.rm_rf file_path
+
+    bucket = @s3.buckets["test_metadata"]
+    object = bucket.objects["metaobject"]
+    object.write(
+      'data',
+      # this is sent as header x-amz-storage-class
+      :storage_class => 'REDUCED_REDUNDANCY',
+      # this is sent as header x-amz-meta-custom1
+      :metadata => {
+        "custom1" => "foobar"
+      }
+    )
+    assert object.exists?
+    metadata_file = YAML.load(IO.read("#{file_path}/.fakes3_metadataFFF/metadata"))
+
+    assert metadata_file.has_key?(:custom_metadata), 'Metadata file does not contain a :custom_metadata key'
+    assert metadata_file[:custom_metadata].has_key?('custom1'), ':custom_metadata does not contain field "custom1"'
+    assert_equal 'foobar', metadata_file[:custom_metadata]['custom1'], '"custom1" does not equal expected value "foobar"'
+
+    assert metadata_file.has_key?(:amazon_metadata), 'Metadata file does not contain an :amazon_metadata key'
+    assert metadata_file[:amazon_metadata].has_key?('storage-class'), ':amazon_metadata does not contain field "storage-class"'
+    assert_equal 'REDUCED_REDUNDANCY', metadata_file[:amazon_metadata]['storage-class'], '"storage-class" does not equal expected value "REDUCED_REDUNDANCY"'
+
+  end
 end
