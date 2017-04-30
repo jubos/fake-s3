7170d03957a26e1571a3a12e8beb113aae366c94
J. R. Pickhardt <pickhardt@gmail.com>
2017-04-30 13:01:23 -0700

    Remove extra empty line

--- test/s3_commands_test.rb
+++ test/s3_commands_test.rb
@@ -108,8 +108,7 @@ class S3CommandsTest < Test::Unit::TestCase
     assert_equal "one", obj.metadata[:param1]
     assert_equal "two, three", obj.metadata[:param2]
   end
-
-
+  
   def test_multi_directory
     bucket = Bucket.create("ruby_aws_s3")
     S3Object.store("dir/myfile/123.txt","recursive","ruby_aws_s3")
